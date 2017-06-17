547949151f6d8e4345f297858b8a6abab51b3d8d
Jordan Kniest <jkniest@gena-clusters.net>
2017-06-17 00:03:30 +0000

    Logout button

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -4,11 +4,45 @@
 
     <div id="overview" class="m4t m1t-xs p1">
 
-        @component('partials.panel', ['title' => 'All servers'])
+        @component('partials.panel')
+
+            @slot('title')
+
+                <div class="level">
+
+                    <div class="level-left">
+
+                        <div class="level-item">
+
+                            All servers
+
+                        </div> {{-- div.level-item --}}
+
+                    </div> {{-- div.level-left --}}
+
+                    <div class="level-right">
+
+                        <div class="level-item">
+
+                            <form action="{{route('logout')}}" id="logoutForm" method="post">
+                                {{csrf_field()}}
+                            </form>
+
+                            <a href="#" onclick="$('#logoutForm').submit();">
+                                [Logout]
+                            </a>
+
+                        </div> {{-- div.level-item --}}
+
+                    </div> {{-- div.level-right --}}
+
+                </div> {{-- div.level --}}
+
+            @endslot {{-- slot: title --}}
 
             @include('partials.overview.average')
 
-        @endcomponent
+        @endcomponent {{-- component: panel --}}
 
         @include ('partials.overview.all-servers')
 
